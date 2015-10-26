499a2e817e3d7176527db7ec33f0f321696f0be7
Nukeador <nukeador@gmail.com>
2015-10-26 23:49:14 +0100

    parse jsong when ready

--- js/discourse-api.js
+++ js/discourse-api.js
@@ -7,7 +7,7 @@ DiscourseApi.prototype.category = function (id, cb) {
     var object = new XMLHttpRequest();
     object.open("GET", this.url + '/c/' + id + '.json');
     object.onreadystatechange = function() {
-        if (XMLHttpRequest.DONE != object.readyState) {
+        if (object.readyState === 4 && object.status === 200) {
             cb(JSON.parse(object.responseText));
         }
     };
@@ -18,7 +18,7 @@ DiscourseApi.prototype.topic = function (id, cb) {
     var object = new XMLHttpRequest();
     object.open("GET", this.url + '/t/' + id + '.json');
     object.onreadystatechange = function() {
-        if (XMLHttpRequest.DONE != object.readyState) {
+        if (object.readyState === 4 && object.status === 200) {
             cb(JSON.parse(object.responseText));
         }
     };
